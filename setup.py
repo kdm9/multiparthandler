0efd14589cdae650b9fb00098b77b71427091791
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-10 18:28:22 +1100

    fixed license classifier in setup.py

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
-        ("License :: OSI Approved :: GNU Library or Lesser General Public"
-        "License (LGPL)"),
+        "License :: OSI Approved :: GNU Lesser General Public License v3 "
+        "(LGPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
