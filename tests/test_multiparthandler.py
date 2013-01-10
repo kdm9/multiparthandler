b683e50fa7f858c165e450ee6c8b623118dd1b1a
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-11 10:42:53 +1100

    possible fix for lack of bytes() in py2.5

--- tests/test_multiparthandler.py
+++ tests/test_multiparthandler.py
@@ -24,8 +24,11 @@ class multiparthandler_t(unittest.TestCase):
         # Some inter-version portability here.
         # Python 2 returns a str from .read(), py3 returns bytes. if we convert
         # this to bytes, then we can reproducibly decode it to a string.
-        self.test_html = bytes(self.opener.open(self.test_url).read()).\
+        try:
+            self.test_html = bytes(self.opener.open(self.test_url).read()).\
                 decode("UTF-8")
+        except NameError:  # If the bytes() fn is not found, i.e. py2.5
+            self.test_html = self.opener.open(self.test_url).read()
 
     def test_post_as_file(self):
         tmp_fd, tmp_fn = tempfile.mkstemp(suffix=".html")
@@ -37,8 +40,12 @@ class multiparthandler_t(unittest.TestCase):
             "uploaded_file": open(tmp_fn, "r")
             }
         # Portablity hack as above
-        response_html = bytes(self.opener.open(self.validator_url, params).
-                read()).decode("UTF-8")
+        try:
+            response_html = bytes(self.opener.open(self.validator_url, params).
+                    read()).decode("UTF-8")
+        except NameError:
+            response_html = self.opener.open(self.validator_url, params)\
+                    .read()
         os.remove(tmp_fn)
         re_match = re.search(self.validator_result_re, response_html)
         self.assertTrue(re_match is not None)
@@ -50,8 +57,12 @@ class multiparthandler_t(unittest.TestCase):
             "fragment": self.test_html
             }
         # Portablity hack as above
-        response_html = bytes(self.opener.open(self.validator_url, params).
-                read()).decode("UTF-8")
+        try:
+            response_html = bytes(self.opener.open(self.validator_url, params).
+                    read()).decode("UTF-8")
+        except NameError:
+            response_html = self.opener.open(self.validator_url, params)\
+                    .read()
         re_match = re.search(self.validator_result_re, response_html)
         self.assertTrue(re_match is not None)
 
