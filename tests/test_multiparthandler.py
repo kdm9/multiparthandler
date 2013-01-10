17a21c7553ff9889d5586f0487ec0ee50078efe2
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-10 23:05:51 +1100

    added with import, so py2.5 does not fail

--- tests/test_multiparthandler.py
+++ tests/test_multiparthandler.py
@@ -10,7 +10,7 @@ except ImportError:
     import urllib.request as a_urllib
 import re
 from multiparthandler import multiparthandler
-
+from __future__ import with_statement
 
 class multiparthandler_t(unittest.TestCase):
     validator_url = "http://validator.w3.org/check"
